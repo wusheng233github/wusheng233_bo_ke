fa578597928f4f3458eb521368609e4b2c48a2df
wusheng233 <wusheng233@qq.com>
2023-12-27 19:24:38 +0800

    4个空的style看着难受

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ echo getconfig("config.txt", "标题:");
 <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
-<div class="navbar-collapse collapse" id="navbarColor03" style="">
+<div class="navbar-collapse collapse" id="navbarColor03">
 <ul class="navbar-nav me-auto">
 <li class="nav-item">
 <a class="nav-link active" href="index.php">首页
